7500af833bb8ffd60b8fb040ee74d9148a792f13
Ilham Pratama <pilham902@gmail.com>
2025-06-03 22:39:52 +0700

    feat: add background music functionality to the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './App.css'
 
 function App() {
   const [time, setTime] = useState(new Date());
+  const audioRef = useRef(null);
 
   useEffect(() => {
     const timer = setInterval(() => {
@@ -20,18 +21,33 @@ function App() {
   const formatDate = (time) => {
     return time.toLocaleDateString("id-ID", { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
   }
+
+  const handlePlayMusic = () => {
+    if (audioRef.current) {
+      audioRef.current.play();
+    }
+  }
+
   return (
     <div
       className="h-screen w-screen flex flex-col justify-center items-center pb-80"
       style={{
-        backgroundImage: "url('/wall1.jpg')",
+        backgroundImage: "url('wall1.jpg')",
         backgroundSize: "cover",
         backgroundPosition: "center",
         color: "#cbd5e1",
       }}
     >
+      <audio ref={audioRef} loop>
+        <source src="bgMusic.mp3" type="audio/mp3" />
+        Your browser does not support the audio element.
+      </audio>
       <h1 className="text-xl mb-4">{formatDate(time)}</h1>
       <h2 className="text-[120px] font-light">{formatTime(time)}</h2>
+      <button
+          onClick={handlePlayMusic}
+          className="mt-8 px-4 py-2 bg-white text-black rounded-lg shadow"
+      ></button>
     </div>
   )
 }
